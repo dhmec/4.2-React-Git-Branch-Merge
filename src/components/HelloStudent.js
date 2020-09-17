2dbd8e9ab559384a3f4d62427fffcf0f45bcbb0c
dhmec <dharmoradiya@gmail.com>
2020-09-17 11:12:23 -0600

    created hello student component

---
+++ src/components/HelloStudent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HelloStudent = (props) => 
+{
+    return(
+        <p>Hello, {props.name}!</p>
+    );
+}
+
+export default HelloStudent;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloStudent from './components/HelloStudent';
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <h1>My Git Test</h1>
+    <HelloStudent name='Dhar' />
+    <HelloStudent name='Jigs' />
   </React.StrictMode>,
   document.getElementById('root')
 );
